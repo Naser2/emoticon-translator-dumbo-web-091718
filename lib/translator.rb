3510f47549629fb8b21fdcbe3824e5b84c582a03
Nasser Sanou <nassersanou23@gmail.com>
2018-09-04 02:06:17 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,34 @@
+require 'yaml'
+require 'pry'
+# YAML.load_file('emoticons.yml')
+# puts thing.inspect
 # require modules here
 
-def load_library
-  # code goes here
-end
+def load_library(file_pah)
+  library = { "get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file("./lib/emoticons.yml").each do |meaning, emoticon_array_when_loaded|
+    english, japanese = emoticon_array_when_loaded
+     library['get_meaning'][japanese] = meaning
+     library['get_emoticon'][english] = japanese 
+   end
+   library
+end 
+
+# def load_library(file_pah)
+#   emoticon_lib = {'get_meaning'  => {},
+#                   'get_emoticon' => {} }
+#   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+#     english = value[0]
+#     japanese = value[1]
+#     emoticon_lib['get_meaning'][japanese] = meaning
+#     emoticon_lib['get_emoticon'][english] = japanese
+#   end
+#   emoticon_lib
+# end
+
 
 def get_japanese_emoticon
-  # code goes here
+ YAML.load_file(file_path).each do |meaning 
 end
 
 def get_english_meaning
